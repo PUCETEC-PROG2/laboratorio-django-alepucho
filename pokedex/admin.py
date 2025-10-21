0eb779ba66d90177b14acd2d976b130611adeef3
LABORATORIO FELIPE SEGOVIA <laboratorio.fs@ad.uisek.edu.ec>
2025-10-21 11:02:49 -0500

    Segundo Commit

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Pokemon
+from .models import Trainer
 @admin.register(Pokemon)
-class PokemosAdmin(admin.ModelAdmin):
+class PokemonsAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Trainer)
+class TrainersAdmin(admin.ModelAdmin):
     pass
--- pokedex/models.py
+++ pokedex/models.py
@@ -5,6 +5,14 @@ class Pokemon(models.Model):
     type = models.CharField(max_length=40, null=False)
     weight = models.IntegerField(null=False)
     height = models.IntegerField(null=False)
+    def __str__(self):
+        return self.name
     
+class Trainer (models.Model):
+    name=models.CharField(max_length=20, null=False)
+    last_name=models.CharField(max_length=20, null=False)
+    years=models.IntegerField(null=False)
+    level=models.IntegerField(null=False)
+    datebirth=models.DateField(null=False)
     def __str__(self):
         return self.name
