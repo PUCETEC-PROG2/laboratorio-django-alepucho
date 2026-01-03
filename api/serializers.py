b4b5e208851094081ccc85b6a9c66cbb95537d71
alepucho <alepucho06@gmail.com>
2026-01-03 18:21:52 -0500

    Cambios 3-1-2026

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from pokedex.models import Pokemon
+from pokedex.models import Pokemon, Trainer
 from django.core.files.base import ContentFile
 import base64
 class PokemonSerializer(serializers.ModelSerializer):
@@ -23,3 +23,9 @@ class PokemonSerializer(serializers.ModelSerializer):
             except Exception:
                 raise serializers.ValidationError("Invalid base64 image")
         return None
+
+
+class TrainerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Trainer
+        fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from .serializers import PokemonSerializer
-from pokedex.models import Pokemon
+from .serializers import PokemonSerializer, TrainerSerializer
+from pokedex.models import Pokemon, Trainer
 from oauth2_provider.contrib.rest_framework import TokenHasScope, OAuth2Authentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
@@ -11,6 +11,18 @@ class PokemonViewSet(viewsets.ModelViewSet):
     authentication_classes = [OAuth2Authentication]
     required_scopes = ['write']
     
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PUT', 'DELETE']:
+            return [IsAuthenticated(), TokenHasScope()]
+        return [AllowAny()]
+
+
+class TrainerViewSet(viewsets.ModelViewSet):
+    queryset = Trainer.objects.all()
+    serializer_class = TrainerSerializer
+    authentication_classes = [OAuth2Authentication]
+    required_scopes = ['write']
+
     def get_permissions(self):
         if self.request.method in ['POST', 'PUT', 'DELETE']:
             return [IsAuthenticated(), TokenHasScope()]
